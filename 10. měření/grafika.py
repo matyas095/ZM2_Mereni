3740f1b013d35c0932a805bb569161a027049c9c
Matyas Kutaj <matyas.kutaj@tldr-it.com>
2026-04-07 23:15:00 +0200

    Finalni impl 10. mereni - 3. ukol

--- 10. měření/grafika.py
+++ 10. měření/grafika.py
@@ -1,13 +1,13 @@
 # To RUN: python3 [py] graf_vypocty.py -i INPUT_FILE_PATH
-import json
+
 import re;
 import argparse;
 from pathlib import Path;
 import numpy as np;
 import math;
 import matplotlib.pyplot as plt;
-import sys;
 import os;
+import sys;
 
 parent_dir = Path(__file__).resolve().parent;
 script_dir = parent_dir / "grafy";
@@ -92,37 +92,84 @@ def main():
 
     parsedDataPath = parent_dir / "parsedData";
 
-    # try:
-    onlyfiles = [parsedDataPath / f for f in os.listdir(parsedDataPath) if os.path.isfile(os.path.join(parsedDataPath, f))];
-    hodnoty = convertFile(onlyfiles);
-    for file_name, vals in hodnoty.items():
-        splitName = file_name.split("_")[1];
-        num = file_name.split("_")[-1].split(".")[0];
-        for velicina, val in vals.items():
-            val_hod = val["hodnoty"];
-            if(velicina == "X" or velicina == "Time [s]"):
-                x_Range = val_hod;
-                x_Key = "t [s]";
-            elif(velicina == "Y"):
-                y_Range = val_hod;
+    try:
+        onlyfiles = [parsedDataPath / f for f in os.listdir(parsedDataPath) if os.path.isfile(os.path.join(parsedDataPath, f))];
+        hodnoty = convertFile(onlyfiles);
+        for file_name, vals in hodnoty.items():
+            splitName = file_name.split("_")[1];
+            num = file_name.split("_")[-1].split(".")[0];
+            for velicina, val in vals.items():
+                val_hod = val["hodnoty"];
+                if(velicina == "X" or velicina == "Time [s]"):
+                    x_Range = val_hod;
+                    x_Key = "t [s]";
+                elif(velicina == "Y"):
+                    y_Range = val_hod;
 
-        if(splitName == "E"): title = "Měření celkové energie E vozíček č. " + num;
-        elif(splitName == "U"): title = "Měření potenciální energie U vozíček č. " + num;
-        elif(splitName == "T"): title = "Měření kinetické energie T vozíček č. " + num;
-        else: raise ValueError("Chyba v datech ig..");
+            if(splitName == "E"): title = "Měření celkové energie E vozíček č. " + num;
+            elif(splitName == "U"): title = "Měření potenciální energie U vozíček č. " + num;
+            elif(splitName == "T"): title = "Měření kinetické energie T vozíček č. " + num;
+            else: raise ValueError("Chyba v datech ig..");
 
-        y_Key = splitName + " [J]";
+            y_Key = splitName + " [J]";
 
-        doGraph_SCATTER(x_Range, y_Range, x_Key, y_Key, title);
-        save_Graph_And_Leave(file_name)
+            if(any(v is None for v in [x_Range, y_Range, x_Key, y_Key])): raise ValueError("Chyba v nepridanych datech..");
 
+            doGraph_SCATTER(x_Range, y_Range, x_Key, y_Key, title);
+            save_Graph_And_Leave(file_name);
 
-    """except FileNotFoundError:
+        # Ukol 3
+        print();
+        print("=" * 150);
+        print();
+
+        mereni_1 = np.array([0.124, 0.138, 0.137]) * 2;
+        mereni_2 = np.array([0.753, 0.673, 0.722]) * 2;
+        mereni_3 = np.array([0.660, 0.761, 0.737]) * 2;
+        mereni_4 = np.array([0.132, 0.132, 0.135]) * 2;
+
+        suma_mereni_1 = np.sum(mereni_1) / len(mereni_1);
+        suma_mereni_2 = np.sum(mereni_2) / len(mereni_2);
+        suma_mereni_3 = np.sum(mereni_3) / len(mereni_3);
+        suma_mereni_4 = np.sum(mereni_4) / len(mereni_4);
+        chyba_mereni_1 = np.std(mereni_1) / len(mereni_1);
+        chyba_mereni_2 = np.std(mereni_2) / len(mereni_2);
+        chyba_mereni_3 = np.std(mereni_3) / len(mereni_3);
+        chyba_mereni_4 = np.std(mereni_4) / len(mereni_4);
+
+        zavazi1  = 0.008;  # kg
+        zavazi2  = 0.049;  # kg
+        vozicek1 = 0.215; # kg
+        vozicek2 = 0.213; # kg
+
+        def zrychleni_vozicku(m_kladka, M_vozicek):
+            return (m_kladka * 9.81) / (M_vozicek + m_kladka);
+
+        def chyba_zrychleni_vozicku(m_kladka, chyba_kladka, M_vozicek, chyba_vozicek):
+            return 9.81 / ((M_vozicek + m_kladka) ** 2) * np.sqrt((M_vozicek * chyba_vozicek) ** 2 + (m_kladka * chyba_kladka) ** 2);
+
+        def doThingys(zavazi, vozicek, suma, chyba_mereni, text):
+            teoreticke_zrychleni_vozicek1_zavazi1 = zrychleni_vozicku(zavazi, vozicek);
+            chyba_teoreticke_1_1 = chyba_zrychleni_vozicku(zavazi, 0.005, vozicek, 0.005);
+            print(color_print.GREEN + color_print.BOLD + text + color_print.END);
+            print(f"├──{color_print.UNDERLINE}Namerene{color_print.END}: ({suma:.2f} ± {chyba_mereni:.2f}) ms^-1");
+            print(f"└────{color_print.UNDERLINE}Teoreticke{color_print.END}: ({teoreticke_zrychleni_vozicek1_zavazi1:.2f} ± {chyba_teoreticke_1_1:.2f}) ms^-1");
+            print();
+
+        # Vozicek 1.
+        doThingys(zavazi1, vozicek1, suma_mereni_1, chyba_mereni_1, "Vozicek 1. (0.215kg) zavazi 1. (0.008kg)");
+        doThingys(zavazi2, vozicek1, suma_mereni_2, chyba_mereni_2, "Vozicek 1. (0.215kg) zavazi 2. (0.049kg)");
+
+        # Vozicek 2.
+        doThingys(zavazi1, vozicek2, suma_mereni_4, chyba_mereni_4, "Vozicek 2. (0.213kg) zavazi 1. (0.008kg)");
+        doThingys(zavazi2, vozicek2, suma_mereni_3, chyba_mereni_3, "Vozicek 2. (0.213kg) zavazi 2. (0.049kg)");
+
+    except FileNotFoundError:
         print(f"Error: The file '{args.input}' was not found.");
         sys.exit(1);
     except Exception as e:
         print(f"An error occurred: {e}");
-        sys.exit(1);"""
+        sys.exit(1);
 
 
 if __name__ == "__main__":
